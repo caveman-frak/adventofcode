c1a239468630b0cffffdc575b27ec52ff8f5327a
Caveman Frak <tim.pickard@bluegecko.co.uk>
2021-12-02 19:16:44 +0000

    day 1 puzzle 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,8 +8,9 @@ use std::{
 fn main() -> Result<(), Error> {
     let depths = Depths::new(false, file(Path::new("day1/test/input.txt"))?);
 
-    let variance = depths.variances();
-    let count = depths.count(&variance, &Variance::Increased);
+    let windows = depths.windows(depths.depths());
+    let variances = depths.variances(&windows);
+    let count = depths.count(&variances, &Variance::Increased);
     println!("No of increases = {}", count);
 
     Ok(())
@@ -38,11 +39,32 @@ impl Depths {
         Depths { debug, depths }
     }
 
-    fn variances(&self) -> Vec<Variance> {
+    fn depths(&self) -> &Vec<u32> {
+        &self.depths
+    }
+
+    fn windows(&self, depths: &Vec<u32>) -> Vec<u32> {
+        let mut windows = Vec::new();
+        let mut window = Window::new();
+
+        for current in depths {
+            window.push(Some(*current));
+            if window.is_full() {
+                if self.debug {
+                    println!("{}", current);
+                }
+                windows.push(window.sum());
+            }
+        }
+
+        windows
+    }
+
+    fn variances(&self, depths: &Vec<u32>) -> Vec<Variance> {
         let mut variances = Vec::new();
         let mut previous: Option<&u32> = None;
 
-        for current in &self.depths {
+        for current in depths {
             if self.debug {
                 println!("{}", current);
             }
@@ -90,6 +112,39 @@ impl Variance {
     }
 }
 
+#[derive(Debug)]
+struct Window {
+    a: Option<u32>,
+    b: Option<u32>,
+    c: Option<u32>,
+}
+
+impl Window {
+    fn new() -> Self {
+        Self {
+            a: None,
+            b: None,
+            c: None,
+        }
+    }
+
+    fn push(&mut self, item: Option<u32>) {
+        self.a = self.b;
+        self.b = self.c;
+        self.c = item;
+    }
+
+    fn sum(&self) -> u32 {
+        self.a.unwrap_or(0) + self.b.unwrap_or(0) + self.c.unwrap_or(0)
+    }
+
+    fn is_full(&self) -> bool {
+        self.a.map_or(false, |_| true)
+            && self.b.map_or(false, |_| true)
+            && self.c.map_or(false, |_| true)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use {super::*, std::io::Cursor};
@@ -121,10 +176,10 @@ mod tests {
 
     #[test]
     fn check_variance() {
-        let depth = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+        let depths = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
 
         assert_eq!(
-            depth.variances(),
+            depths.variances(depths.depths()),
             vec![
                 Variance::NotApplicable,
                 Variance::Increased,
@@ -142,10 +197,49 @@ mod tests {
 
     #[test]
     fn check_count() {
-        let depth = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
-        let variances = depth.variances();
+        let depths = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+        let variances = depths.variances(depths.depths());
 
-        assert_eq!(depth.count(&variances, &Variance::Increased), 7);
+        assert_eq!(depths.count(&variances, &Variance::Increased), 7);
+    }
+
+    #[test]
+    fn check_windows() {
+        let depths = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+
+        assert_eq!(
+            depths.windows(depths.depths()),
+            vec![607, 618, 618, 617, 647, 716, 769, 792]
+        )
+    }
+
+    #[test]
+    fn check_window_variances() {
+        let depths = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+        let windows = depths.windows(depths.depths());
+
+        assert_eq!(
+            depths.variances(&windows),
+            vec![
+                Variance::NotApplicable,
+                Variance::Increased,
+                Variance::Unchanged,
+                Variance::Decreased,
+                Variance::Increased,
+                Variance::Increased,
+                Variance::Increased,
+                Variance::Increased,
+            ]
+        )
+    }
+
+    #[test]
+    fn check_window_count() {
+        let depths = Depths::new(true, vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
+        let windows = depths.windows(depths.depths());
+        let variances = depths.variances(&windows);
+
+        assert_eq!(depths.count(&variances, &Variance::Increased), 5)
     }
 
     #[test]
