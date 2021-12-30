72d9a4ff2946c9e6646532f4215a9b457a2886e6
Caveman Frak <tim.pickard@bluegecko.co.uk>
2021-12-30 14:20:27 +0000

    day 8 puzzle 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,25 +1,26 @@
 use {
     anyhow::Result,
     common::input::{from_path, inputs},
+    std::collections::{HashMap, HashSet},
 };
 
 fn main() -> Result<()> {
-    let inputs = inputs(to_usize, from_path("day8/data/input.txt")?);
-
-    part1(&inputs)?;
-    part2(&inputs)?;
+    part1("day8/data/input.txt")?;
+    part2("day8/data/input.txt")?;
 
     Ok(())
 }
 
-fn part1(inputs: &[usize]) -> Result<()> {
+fn part1(path: &str) -> Result<()> {
+    let inputs = inputs(to_usize, from_path(path)?);
     println!("Day 7 Part 1 => {:?}", inputs.iter().sum::<usize>());
 
     Ok(())
 }
 
-fn part2(_inputs: &[usize]) -> Result<()> {
-    println!("Day 8 Part 2 => ");
+fn part2(path: &str) -> Result<()> {
+    let inputs = inputs(to_usize2, from_path(path)?);
+    println!("Day 7 Part 2 => {:?}", inputs.iter().sum::<usize>());
 
     Ok(())
 }
@@ -27,10 +28,16 @@ fn part2(_inputs: &[usize]) -> Result<()> {
 #[derive(Debug, PartialEq)]
 enum Digit {
     Unknown,
+    Zero,
     One,
+    Two,
+    Three,
     Four,
+    Five,
+    Six,
     Seven,
     Eight,
+    Nine,
 }
 
 impl Digit {
@@ -43,6 +50,50 @@ impl Digit {
             _ => None,
         }
     }
+
+    fn decode<T: Into<String>>(segment: &Segment, s: T) -> Option<Digit> {
+        let s = sort(s);
+
+        if s == segment.zero {
+            Some(Digit::Zero)
+        } else if s == segment.one {
+            Some(Digit::One)
+        } else if s == segment.two {
+            Some(Digit::Two)
+        } else if s == segment.three {
+            Some(Digit::Three)
+        } else if s == segment.four {
+            Some(Digit::Four)
+        } else if s == segment.five {
+            Some(Digit::Five)
+        } else if s == segment.six {
+            Some(Digit::Six)
+        } else if s == segment.seven {
+            Some(Digit::Seven)
+        } else if s == segment.eight {
+            Some(Digit::Eight)
+        } else if s == segment.nine {
+            Some(Digit::Nine)
+        } else {
+            None
+        }
+    }
+
+    fn value(&self) -> usize {
+        match self {
+            Digit::Zero => 0,
+            Digit::One => 1,
+            Digit::Two => 2,
+            Digit::Three => 3,
+            Digit::Four => 4,
+            Digit::Five => 5,
+            Digit::Six => 6,
+            Digit::Seven => 7,
+            Digit::Eight => 8,
+            Digit::Nine => 9,
+            _ => panic!(),
+        }
+    }
 }
 
 impl Default for Digit {
@@ -51,14 +102,166 @@ impl Default for Digit {
     }
 }
 
+fn sort<'a, T: Into<String>>(s: T) -> String {
+    let s = s.into();
+    let mut v: Vec<char> = s.trim().chars().collect();
+    v.sort();
+    v.iter().collect::<String>()
+}
+
+#[derive(Debug)]
+struct Segment {
+    zero: String,
+    one: String,
+    two: String,
+    three: String,
+    four: String,
+    five: String,
+    six: String,
+    seven: String,
+    eight: String,
+    nine: String,
+}
+
+impl Segment {
+    fn new<T: Into<String>>(
+        zero: T,
+        one: T,
+        two: T,
+        three: T,
+        four: T,
+        five: T,
+        six: T,
+        seven: T,
+        eight: T,
+        nine: T,
+    ) -> Self {
+        Self {
+            zero: sort(zero),
+            one: sort(one),
+            two: sort(two),
+            three: sort(three),
+            four: sort(four),
+            five: sort(five),
+            six: sort(six),
+            seven: sort(seven),
+            eight: sort(eight),
+            nine: sort(nine),
+        }
+    }
+
+    fn solve(inputs: &[&str]) -> Self {
+        let mut zero: Option<String> = None;
+        let mut one: Option<String> = None;
+        let mut two: Option<String> = None;
+        let mut three: Option<String> = None;
+        let mut four: Option<String> = None;
+        let mut five: Option<String> = None;
+        let mut six: Option<String> = None;
+        let mut seven: Option<String> = None;
+        let mut eight: Option<String> = None;
+        let mut nine: Option<String> = None;
+
+        let mut map: HashMap<usize, Vec<HashSet<char>>> = HashMap::new();
+
+        for s in inputs {
+            match s.len() {
+                2 => one = Some(s.to_string()),
+                3 => seven = Some(s.to_string()),
+                4 => four = Some(s.to_string()),
+                7 => eight = Some(s.to_string()),
+                _ => map
+                    .entry(s.len())
+                    .or_default()
+                    .push(s.chars().collect::<HashSet<char>>()),
+            }
+        }
+        let one_set: HashSet<char> = one.as_ref().unwrap().chars().collect();
+        let four_minus_one_set: HashSet<char> = four
+            .as_ref()
+            .unwrap()
+            .chars()
+            .collect::<HashSet<char>>()
+            .difference(&one_set)
+            .copied()
+            .collect();
+        let mut v = map.get(&5).unwrap().clone();
+        while let Some(s) = v.pop() {
+            // 2,3 or 5
+            if three.is_none() && s.is_superset(&one_set) {
+                // & 1 = 3
+                three = Some(s.iter().collect());
+            } else if five.is_none() && s.is_superset(&four_minus_one_set) {
+                // & 4 - 1 = 5
+                five = Some(s.iter().collect());
+            } else if three.is_some() && five.is_some() {
+                // = 2
+                two = Some(s.iter().collect());
+            } else {
+                v.insert(0, s);
+            }
+        }
+        let three_set: HashSet<char> = three.as_ref().unwrap().chars().collect();
+        let mut v = map.get(&6).unwrap().clone();
+        while let Some(s) = v.pop() {
+            // 0, 6 or 9
+            if nine.is_none() && s.is_superset(&three_set) {
+                // & 3 = 9
+                nine = Some(s.iter().collect());
+            } else if zero.is_none() && s.is_superset(&one_set) {
+                // & 1 = 0
+                zero = Some(s.iter().collect());
+            } else if nine.is_some() && zero.is_some() {
+                // = 6
+                six = Some(s.iter().collect());
+            } else {
+                v.insert(0, s);
+            }
+        }
+
+        Self::new(
+            zero.unwrap(),
+            one.unwrap(),
+            two.unwrap(),
+            three.unwrap(),
+            four.unwrap(),
+            five.unwrap(),
+            six.unwrap(),
+            seven.unwrap(),
+            eight.unwrap(),
+            nine.unwrap(),
+        )
+    }
+}
+
+impl Default for Segment {
+    fn default() -> Self {
+        Self::new(
+            "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg",
+        )
+    }
+}
+
 fn to_usize(s: String) -> Option<usize> {
-    //None
     s.split('|').skip(1).next().map(|s| {
         s.trim()
             .split(' ')
-            .map(|s| Digit::unique(s.trim().len() as u32))
-            .filter(|v| v.is_some())
+            .filter_map(|s| Digit::unique(s.trim().len() as u32))
             .count()
+    })
+}
+
+fn to_usize2(s: String) -> Option<usize> {
+    let mut i = s.split('|');
+    let segment = Segment::solve(&i.next().unwrap().split(' ').collect::<Vec<&str>>());
+    i.next().map(|s| {
+        s.trim()
+            .split(' ')
+            .filter_map(|s| Digit::decode(&segment, s))
+            .fold(0usize, |mut acc, v| {
+                acc = (acc * 10) + v.value();
+                acc
+            })
     })
 }
 
@@ -77,9 +280,60 @@ mod tests {
     }
 
     #[test]
-    fn check_to_u32() {
+    fn check_to_usize() {
         let s =
             "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc";
         assert_eq!(to_usize(s.to_string()), Some(3));
+    }
+
+    #[test]
+    fn check_to_usize_v2() {
+        let s =
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+        assert_eq!(to_usize2(s.to_string()), Some(5353));
+    }
+
+    #[test]
+    fn check_solve() {
+        let s =
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+        let mut i = s.split('|');
+        let segment = Segment::solve(&i.next().unwrap().split(' ').collect::<Vec<&str>>());
+        assert_eq!(segment.zero, "abcdeg".to_string());
+        assert_eq!(segment.one, "ab".to_string());
+        assert_eq!(segment.two, "acdfg".to_string());
+        assert_eq!(segment.three, "abcdf".to_string());
+        assert_eq!(segment.four, "abef".to_string());
+        assert_eq!(segment.five, "bcdef".to_string());
+        assert_eq!(segment.six, "bcdefg".to_string());
+        assert_eq!(segment.seven, "abd".to_string());
+        assert_eq!(segment.eight, "abcdefg".to_string());
+        assert_eq!(segment.nine, "abcdef".to_string());
+    }
+
+    #[test]
+    fn check_decode() {
+        let segment = Segment::new(
+            "cagedb", "ab", "gcdfa", "fbcad", "eafb", "cdfbe", "cdfgeb", "dab", "acedgfb", "cefabd",
+        );
+        assert_eq!(Digit::decode(&segment, "aedgfb"), None);
+        assert_eq!(Digit::decode(&segment, "cagedb"), Some(Digit::Zero));
+        assert_eq!(Digit::decode(&segment, "ba"), Some(Digit::One));
+        assert_eq!(Digit::decode(&segment, "gcdfa"), Some(Digit::Two));
+        assert_eq!(Digit::decode(&segment, "fbcad"), Some(Digit::Three));
+        assert_eq!(Digit::decode(&segment, "eafb"), Some(Digit::Four));
+        assert_eq!(Digit::decode(&segment, "cdfbe"), Some(Digit::Five));
+        assert_eq!(Digit::decode(&segment, "cdfgeb"), Some(Digit::Six));
+        assert_eq!(Digit::decode(&segment, "dab"), Some(Digit::Seven));
+        assert_eq!(Digit::decode(&segment, "acedgfb"), Some(Digit::Eight));
+        assert_eq!(Digit::decode(&segment, "cefabd"), Some(Digit::Nine));
+    }
+
+    #[test]
+    fn check_inputs() -> Result<()> {
+        let inputs = inputs(to_usize2, from_path("test/test.txt")?);
+        assert_eq!(inputs.iter().sum::<usize>(), 61229);
+
+        Ok(())
     }
 }
